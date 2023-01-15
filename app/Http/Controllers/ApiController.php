0bfac09a51086a5367f5aaa21bbf74d1572be5dc
vespageorge <viespe.georgesorin@gmail.com>
2023-01-15 02:19:45 +0200

    REMOTE-9: Update run_cmd to use all command_types

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,7 +8,14 @@ use Illuminate\Support\Facades\DB;
 class ApiController extends Controller
 {
 	public function api_get_commands($id) {
-		return DB::table('command_exec')->join('command_default_list', 'command_exec.id_command', '=', 'command_default_list.id')->join('setup', 'setup.id', '=', 'command_exec.id_user')->select('command_default_list.cmd', 'command_exec.id')->where('executed', '=', '0')->where('setup.id', '=', $id)->get();
+		return DB::table('command_exec')
+		->join('command_default_list', 'command_exec.id_command', '=', 'command_default_list.id')
+		->join('setup', 'setup.id', '=', 'command_exec.id_user')
+		->join('command_types','command_types.id','=','command_default_list.command_type_id')
+		->select('command_default_list.cmd', 'command_exec.id', 'command_default_list.command_type_id','command_types.name')
+		->where('executed', '=', '0')
+		->where('setup.id', '=', $id)
+		->get();
 	}
 
 	public function api_get_setup($hwid) {
@@ -17,7 +24,14 @@ class ApiController extends Controller
 
 	public function api_post_setup(Request $request) {
 		if (!Controller::find_setup($request->hwid)) {
-			return DB::table('setup')->insert(['platform' => $request->platform, 'hostname' => $request->hostname, 'ip_address' => $request->ip_address, 'mac_address' => $request->mac_address, 'hwid' => $request->hwid]);
+			return DB::table('setup')
+			->insert([
+				'platform' => $request->platform,
+				'hostname' => $request->hostname,
+				'ip_address' => $request->ip_address,
+				'mac_address' => $request->mac_address,
+				'hwid' => $request->hwid
+			]);
 		} else {
 			return DB::table('setup')->where('hwid','=',$request->hwid)->first();
 		}
